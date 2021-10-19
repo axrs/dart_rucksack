c6bdf3def869e195cdd20f10b652e9bec8898a14
Alexander Scott <xander@axrs.io>
2021-10-19 21:03:48 +1000

    call with args

--- dart_rucksack.sh
+++ dart_rucksack.sh
@@ -4,4 +4,5 @@ if [ ! -d '.dart_tool' ] || [ ! -f 'pubspec.lock' ];then
   dart pub get >/dev/null
 fi
 dart run dart_chassis_forge:build >/dev/null
-dart run tool/entry_command.dart @args
+# shellcheck disable=SC2068
+dart run tool/entry_command.dart $@
