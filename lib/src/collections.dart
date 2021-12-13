6e5624617781272b28f762d5ce069a1a8b224a15
Alexander Scott <xander@axrs.io>
2021-12-13 23:00:50 +1000

    Enhancement: Add support for Map.isEmpty

--- lib/src/collections.dart
+++ lib/src/collections.dart
@@ -1,12 +1,22 @@
 import 'booleans.dart';
 import 'objects.dart';
 
-/// True if the provided [ittr] [Iterable] is not null and is [Iterable.isNotEmpty]
+/// True if the provided [ittr] [Iterable] or [Map] is not null and is [Iterable.isNotEmpty]
 ///
 /// `since 0.0.1`
-bool isNotEmpty(final Iterable? ittr) => isNotNull(ittr) && ittr!.isNotEmpty;
+bool isNotEmpty(final dynamic ittr) {
+  if (isNull(ittr)) {
+    return false;
+  } else if (ittr is Map) {
+    return ittr.isNotEmpty;
+  } else if (ittr is Iterable) {
+    return ittr.isNotEmpty;
+  } else {
+    return false;
+  }
+}
 
-/// True if the provided [ittr] [Iterable] is null or [Iterable.isEmpty]
+/// True if the provided [ittr] [Iterable] or [Map] is null or [Iterable.isEmpty]
 ///
 /// `since 0.0.1`
-bool isEmpty(final Iterable? ittr) => isFalse(isNotEmpty(ittr));
+bool isEmpty(final dynamic ittr) => isFalse(isNotEmpty(ittr));
--- test/src/collections_test.dart
+++ test/src/collections_test.dart
@@ -4,13 +4,21 @@ import 'package:test/test.dart';
 void main() {
   test('isEmpty() is true if the list is null or empty', () {
     expect(collections.isEmpty(null), isTrue);
+    expect(collections.isEmpty(<dynamic>{}), isTrue);
     expect(collections.isEmpty([]), isTrue);
+    expect(collections.isEmpty({}), isTrue);
     expect(collections.isEmpty([1]), isFalse);
+    expect(collections.isEmpty(<dynamic>{'key'}), isFalse);
+    expect(collections.isEmpty({'key': 1}), isFalse);
   });
 
   test('isNotEmpty() is true if the list is not empty', () {
     expect(collections.isNotEmpty(null), isFalse);
+    expect(collections.isNotEmpty(<dynamic>{}), isFalse);
     expect(collections.isNotEmpty([]), isFalse);
+    expect(collections.isNotEmpty({}), isFalse);
     expect(collections.isNotEmpty([1]), isTrue);
+    expect(collections.isNotEmpty(<dynamic>{'key'}), isTrue);
+    expect(collections.isNotEmpty({'key': 1}), isTrue);
   });
 }
